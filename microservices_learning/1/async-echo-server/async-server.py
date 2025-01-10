5d40610aea2ff85214af7910cdbf2dfa247c26fe
Primero1800 <Primero@inbox.ru>
2025-01-10 09:52:04 +0300

    added files

--- microservices_learning/1/async-echo-server/async-server.py
+++ microservices_learning/1/async-echo-server/async-server.py
@@ -1,39 +1,40 @@
 import asyncio
 
 async def handler(reader, writer):
-    while (True):
+    serve = True
+    while serve:
+
         data = await reader.read(128)
         if data:
             message = data.decode()
             addr = writer.get_extra_info('peername')
             print("Received %r from %r" % (message, addr))
-            #print(type(data))
 
             message_back = (f"HTTP/1.0 200 OK ** {message} received").encode('utf-8')
             writer.write(message_back)
             await writer.drain()
         else:
-            print("Close the client socket")
+            serve = False
             writer.close()
             await writer.wait_closed()
-            break
+
+    print("Close the client socket")
+
 
 
 async def main():
-    pass
-
-
-
-if __name__ == "__main__":
-    #asyncio.run(main())
-    loop = asyncio.get_event_loop()
-    d = 2
-
-    server = asyncio.start_server(
+    server = await asyncio.start_server(
         host='localhost',
         port=8000,
         client_connected_cb=handler,
     )
 
-    loop.create_task(server)
-    loop.run_forever()
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- microservices_learning/1/async-echo-server/async3.py
+++ microservices_learning/1/async-echo-server/async3.py
@@ -3,33 +3,29 @@ from asyncio import StreamReader, StreamWriter
 
 
 async def main():
-    loop = asyncio.get_running_loop()
-
     reader: StreamReader
     writer: StreamWriter
     reader, writer= await asyncio.open_connection(
         host='localhost',
         port=8000,
-        #'www.google.com', 443, ssl=True
     )
-
 
     escape = False
     while True:
         data = input()
         if data == 'q':
-            data = 'client run'.encode('utf-8')
+            data = 'client escaped'.encode('utf-8')
             escape = True
         else:
             data = data.encode('utf-8')
+
         writer.write(data)
         await writer.drain()
         reply = await reader.read(128)
         print(reply.decode('utf-8'))
+
         if escape:
             break
-
-
 
 
 if __name__ == "__main__":
