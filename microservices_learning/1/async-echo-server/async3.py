acd58a813611e4b145998699204c71e9325d5442
Primero1800 <Primero@inbox.ru>
2025-01-10 10:31:15 +0300

    added files

--- microservices_learning/1/async-echo-server/async3.py
+++ microservices_learning/1/async-echo-server/async3.py
@@ -1,15 +1,26 @@
 import asyncio
 from asyncio import StreamReader, StreamWriter
 
+class StartClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-async def main():
-    reader: StreamReader
-    writer: StreamWriter
-    reader, writer= await asyncio.open_connection(
-        host='localhost',
-        port=8000,
-    )
+    async def __aenter__(self):
+        reader, writer = await asyncio.open_connection(
+            host=self.host,
+            port=self.port,
+        )
+        self.reader = reader
+        self.writer = writer
+        return (reader, writer, )
 
+    async def __aexit__(self, exc_type, exc, tb):
+        self.writer.close()
+        await self.writer.wait_closed()
+
+
+async def start_talking(reader, writer):
     escape = False
     while True:
         data = input()
@@ -17,7 +28,7 @@ async def main():
             data = 'client escaped\n'.encode('utf-8')
             escape = True
         else:
-            data = (data+'\n').encode('utf-8')
+            data = (data + '\n').encode('utf-8')
 
         writer.write(data)
         await writer.drain()
@@ -26,6 +37,11 @@ async def main():
 
         if escape:
             break
+
+
+async def main():
+    async with StartClient(host='localhost', port=8000) as (reader, writer):
+        await start_talking(reader, writer)
 
 
 if __name__ == "__main__":
