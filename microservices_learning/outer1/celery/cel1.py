d180487166825261a17e8c655a72d25fbe6da681
Primero1800 <Primero@inbox.ru>
2025-01-20 10:44:49 +0300

    added celery

---
+++ microservices_learning/outer1/celery/cel1.py
@@ -0,0 +1,26 @@
+from celery import Celery
+import PIL
+from PIL import Image
+
+app = Celery('image_processor', broker='redis://localhost:6379/0')
+
+@app.task
+def resize_image(image_path, output_path, size):
+    with Image.open(image_path) as img:
+        img.thumbnail(size, Image.LANCZOS)
+        img.save(output_path)
+
+@app.task
+def crop_image(image_path, output_path, crop_box):
+    with Image.open(image_path) as img:
+        cropped_img = img.crop(crop_box)
+        cropped_img.save(output_path)
+
+
+resize_image.apply_async(
+    ('1.png', 'resized1.png', (300, 260)),
+)
+
+crop_image.apply_async(
+    ('resized1.png', 'cropped_resized1.png', (100, 100, 200, 200)),
+)
