dd1a0305b73eba66d8b7cf881d2440f0ff4adffd
Primero1800 <Primero@inbox.ru>
2025-01-20 11:15:41 +0300

    added celery

---
+++ microservices_learning/outer1/celery/cel2.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from PIL import Image
+
+app = Celery('cel_processor', broker='redis://localhost:6379/0')
+
+@app.task
+def reporter(img_path):
+    try:
+        with open(img_path, 'rb') as img, open('reporter.txt', 'w') as file:
+            for line in img.readlines():
+                file.write(str(line))
+    except (UnboundLocalError, FileNotFoundError) as error:
+        with open('reporter.txt', 'wb') as file:
+            file.write(f"{img_path}: {error}".encode())
+
+
+reporter.apply_async(args=('11.png', ), )
+
