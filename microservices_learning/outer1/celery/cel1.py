6f09437c97c1971fd88377ca45351be137b564e8
Primero1800 <Primero@inbox.ru>
2025-01-24 12:16:40 +0300

    added celery

--- microservices_learning/outer1/celery/cel1.py
+++ microservices_learning/outer1/celery/cel1.py
@@ -1,7 +1,10 @@
-from asyncio.log import logger
+import logging
+import time
 from functools import wraps
+from logging.handlers import SysLogHandler
 
 from celery import Celery
+from celery.signals import setup_logging
 from PIL import Image
 
 
@@ -12,11 +15,25 @@ app1.conf.broker_connection_always_reconnect = True
 app1.conf.broker_connection_retry_on_startup = True
 
 
+# @setup_logging.connect
+# def setup_worker_logging(**kwargs):
+#     loglevel = kwargs.get('loglevel', logging.DEBUG)
+#
+#     logfile = kwargs.get('logfile', None)  # По умолчанию None
+#     # format = kwargs.get('format',#     #                     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # Установка формата, если необходимо
+#     # colorize = kwargs.get('colorize', False)  # Установка цветового оформления, если необходимо
+#
+#     root = logging.getLogger()
+#     root.setLevel(loglevel)
+#     root.addHandler(SysLogHandler())
+
+
 
 def retry(timeout=10):
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
+            time.sleep(20)
             try:
                 return func(*args, **kwargs)
             except Exception as ex:
@@ -28,6 +45,7 @@ def retry(timeout=10):
 @app1.task(bind=True)
 @retry(timeout=2)
 def resize_image(image_path, output_path, size):
+    time.sleep(20)
     with Image.open(image_path) as img:
         img.thumbnail(size, Image.LANCZOS)
         img.save(output_path)
@@ -36,6 +54,7 @@ def resize_image(image_path, output_path, size):
 @app1.task(bind=True)
 @retry(timeout=5)
 def crop_image(image_path, output_path, crop_box):
+    time.sleep(20)
     with Image.open(image_path) as img:
         cropped_img = img.crop(crop_box)
         cropped_img.save(output_path)
