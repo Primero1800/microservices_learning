ff38b70d95d21db6a3672cee08752e0f12530bc0
Primero1800 <Primero@inbox.ru>
2025-02-05 15:36:33 +0300

    redis_simple

--- microservices_learning/outer1/celery/celeri_AnIv/redis_simple2.py
+++ microservices_learning/outer1/celery/celeri_AnIv/redis_simple2.py
@@ -1,13 +1,32 @@
 import json
 import os
+from dataclasses import asdict
 
 import redis
 from outer1.celery.celeri_AnIv import config
 
 redis_params = config.REDIS_PARAMS
 
+class Kolbas:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__setattr__(key, value)
+
+    def to_json(self):
+        dictionary = {key: self.__getattribute__(key) for key in self.__dict__}
+        dictionary['_class'] = 'Kolbas'
+        return dictionary
+
+    def __repr__(self):
+        return f"Kolbas({', '.join(f'{key}={value}' for key, value in self.__dict__.items())})"
+
+    def __str__(self):
+        return self.__repr__()
+
+
 def get__app_name():
     return os.path.basename(__file__).split('.')[0]
+
 
 def main():
     app_name = get__app_name()
@@ -16,7 +35,17 @@ def main():
             'name': 'Ivan',
             'profession': 'durak',
         }))
-        client.rpush(app_name, 'corowa')
+        client.rpush(app_name, json.dumps('corowa'))
+
+        client.rpush(app_name, json.dumps(['dog', 'cat']))
+
+        client.rpush(app_name, json.dumps([1, '11']))
+
+        client.rpush(app_name, json.dumps(100500))
+
+        kolbas1 = Kolbas(name='Ivan', age=29)
+        d = 2
+        client.rpush(app_name, json.dumps(kolbas1.to_json()))
 
         print(client.lrange(app_name, 0, -1))
 
@@ -28,9 +57,17 @@ def main():
                 item = json.loads(item)
             except json.JSONDecodeError:
                 item = item.decode()
+            if isinstance(item, dict) and '_class' in item:
+                item = class_from_dict(item)
             print(item, type(item))
 
 
+def class_from_dict(data: dict):
+    class_name = data.pop('_class')
+    class_ = globals().get(class_name)
+    if not class_:
+        raise ValueError(f"Класс '{class_name}' не найден.")
+    return class_(**data)
 
 
 if __name__ == "__main__":
